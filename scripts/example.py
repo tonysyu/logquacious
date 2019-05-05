7523a88243431d5cef63978bc4477267732b2314
Tony S Yu <tsyu80@gmail.com>
2019-05-05 18:42:14 -0500

    Update example with alternate logging config

--- scripts/example.py
+++ scripts/example.py
@@ -1,63 +1,94 @@
+#!/usr/bin/env python
+"""
+Description
+"""
+import argparse
 import logging
 
 from logquacious import LogManager
 
 
-logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.DEBUG)
-
-log = LogManager(__name__)
-
-
-with log.context.debug('greetings'):
-    print('Hello!')
+LOG_FORMATS = {
+    'default':
+        '%(asctime)s %(levelname)-8s %(pathname)s:%(lineno)s %(message)s',
+    'simple': '[%(levelname)s] %(message)s',
+}
 
 
-@log.context.info
-def divide(numerator, denominator):
-    if denominator == 0:
-        log.warning('Attempted division by zero. Returning None')
-        return None
-    return numerator / denominator
+def example():
+    log = LogManager(__name__)
 
 
-divide(1, 0)
+    with log.context.debug('greetings'):
+        print('Hello!')
 
 
-with log.and_suppress(ValueError, msg="It's ok, mistakes happen"):
-    raise ValueError('Test error')
+    @log.context.info
+    def divide(numerator, denominator):
+        if denominator == 0:
+            log.warning('Attempted division by zero. Returning None')
+            return None
+        return numerator / denominator
 
 
-log = LogManager(__name__, context_templates={
-    'function.start': 'Called `{label}({arguments})`',
-    'function.finish': 'Return from `{label}`',
-    'context.start.ERROR': '=============== Enter {label} ===============',
-    'context.finish.ERROR': '=============== Exit {label} ===============',
-})
+    divide(1, 0)
 
 
-@log.context.info
-def no_op():
-    pass
+    with log.and_suppress(ValueError, msg="It's ok, mistakes happen"):
+        raise ValueError('Test error')
 
 
-no_op()
+    log = LogManager(__name__, context_templates={
+        'function.start': 'Called `{label}({arguments})`',
+        'function.finish': 'Return from `{label}`',
+        'context.start.ERROR': '=============== Enter {label} ===============',
+        'context.finish.ERROR': '=============== Exit {label} ===============',
+    })
 
 
-@log.context.info(show_args=True)
-def func_with_args(*args):
-    pass
+    @log.context.info
+    def no_op():
+        pass
 
 
-func_with_args('a', 1)
-
-with log.context.error('error context'):
-    print('This should never have happened! What did you do?')
+    no_op()
 
 
-log = LogManager(__name__, context_templates={
-    'context.start': '=============== Enter {label} ===============',
-    'context.finish': '=============== Exit {label} ===============',
-})
+    @log.context.info(show_args=True)
+    def func_with_args(*args):
+        pass
 
-with log.context.debug('greetings'):
-    print('Hello!')
+
+    func_with_args('a', 1)
+
+    with log.context.error('error context'):
+        print('This should never have happened! What did you do?')
+
+
+    log = LogManager(__name__, context_templates={
+        'context.start': '=============== Enter {label} ===============',
+        'context.finish': '=============== Exit {label} ===============',
+    })
+
+    with log.context.debug('greetings'):
+        print('Hello!')
+
+
+
+def main():
+    formatter = argparse.ArgumentDefaultsHelpFormatter
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=formatter)
+    parser.add_argument('--format', default='default',
+                        choices=LOG_FORMATS.keys(),
+                        help="Log format string used for example.")
+
+    args = parser.parse_args()
+
+    format_string = LOG_FORMATS[args.format]
+    logging.basicConfig(format=format_string, level=logging.DEBUG)
+    example()
+
+
+if __name__ == '__main__':
+    main()
